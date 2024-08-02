91faa2d819c0b49674d0b46d5729eb8858dcbf59
baechu805 <bjoo606@naver.com>
2024-08-02 15:04:33 +0900

    수정

--- extract2.py
+++ extract2.py
@@ -14,6 +14,11 @@ from airflow.operators.python import (
     BranchPythonOperator,
     is_venv_installed,
 )
+REQUIREMENTS = [
+        "git+https://github.com/LeeChungBae/Extract_package.git@dev/d1.0.0",
+        "git+https://github.com/LeeChungBae/Transform_package.git@dev/d1.0.0",
+        "git+https://github.com/LeeChungBae/Load_package.git@dev/d1.0.0"
+]
 with DAG(
         'extract2',
     default_args={
@@ -27,10 +32,21 @@ with DAG(
     catchup=True,
     tags=['api', 'movie', 'amt'],
 ) as dag:
+    
+    def ex2():
+        from extract_package.ice_breaking import ice_breaking
+        ice_breaking()
+        
+        
 
     start = EmptyOperator(task_id = 'start')
     end = EmptyOperator(task_id = 'end')
 
-    extract2 = EmptyOperator(task_id = 'extract2')
+    extract2 = PythonVirtualenvOperator(
+        task_id = 'extract2',
+        python_callable = ex2,
+        system_site_packages=False,
+        requirements=REQUIREMENTS[0],
+)
 
 start >> extract2 >> end
